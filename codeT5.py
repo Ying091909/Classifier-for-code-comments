c68c1c05bbf9ef0a853e5ec7549259e2767a844b
Ying091909 <72859301+Ying091909@users.noreply.github.com>
2023-02-13 23:57:36 +0800

    Update codeT5.py

--- codeT5.py
+++ codeT5.py
@@ -244,7 +244,7 @@ def main():
                         help="Epsilon for Adam optimizer.")
     parser.add_argument("--max_grad_norm", default=1.0, type=float,
                         help="Max gradient norm.")
-    parser.add_argument("--num_train_epochs", default=3, type=int,
+    parser.add_argument("--num_train_epochs", default=120, type=int,
                         help="Total number of training epochs to perform.")
     parser.add_argument("--max_steps", default=-1, type=int,
                         help="If > 0: set total number of training steps to perform. Override num_train_epochs.")
